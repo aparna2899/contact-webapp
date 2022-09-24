40722f961fa23642bd5432865f829ca688e436c3
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-09-25 01:47:43 +0530

    create contact-detail page

--- contact/tailwind.config.js
+++ contact/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/Header.js', './src/pages/Home.js'],
+  content: [
+    './src/components/Header.js',
+    './src/pages/Home.js',
+    './src/pages/ContactDetail.js',
+  ],
   theme: {
     extend: {},
   },
