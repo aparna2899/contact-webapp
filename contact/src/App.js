19deb2414037a1c206dcb4ef799d8716d73fe919
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-09-25 23:13:31 +0530

    create message page

--- contact/src/App.js
+++ contact/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import ContactDetails from './pages/ContactDetail';
 import Home from './pages/Home';
+import Message from './pages/Message';
+import MessageDetail from './pages/MessageDetail';
+
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/contact" component={ContactDetails} />
+        <Route exact path="/message" component={Message} />
+        <Route exact path="/messagelist" component={MessageDetail} />
       </Switch>
     </BrowserRouter>
   );
